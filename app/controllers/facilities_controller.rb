5d7f501089f1877463ee120e71c0c75556d6920b
Hayato Saito <omimisama@gmail.com>
2020-02-29 10:56:23 +0900

    #6_施設新規登録機能実装

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,4 +1,27 @@
 class FacilitiesController < ApplicationController
-  def new
+
+  def show
+    @facility = Facility.find(params[:id])
   end
+
+  def new
+    @facility = Facility.new
+  end
+
+  def create
+    @facility = Facility.new(facility_params)
+    if @facility.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @facility
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def facility_params
+      params.require(:facility).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
